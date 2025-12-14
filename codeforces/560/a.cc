5dac8c409f3c0dbc7771788de00e09bae81dd2eb
Barrett Ruth <br.barrettruth@gmail.com>
2025-12-13 18:27:59 -0600

    more contests

---
+++ codeforces/560/a.cc
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>  // {{{
+
+#include <version>
+#ifdef __cpp_lib_ranges_enumerate
+#include <ranges>
+namespace rv = std::views;
+namespace rs = std::ranges;
+#endif
+
+#pragma GCC optimize("O2,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+using namespace std;
+
+using i16 = int16_t;
+using u16 = uint16_t;
+using i32 = int32_t;
+using u32 = uint32_t;
+using i64 = int64_t;
+using u64 = uint64_t;
+using f64 = double;
+using f128 = long double;
+
+#if __cplusplus >= 202002L
+template <typename T>
+constexpr T MIN = std::numeric_limits<T>::min();
+
+template <typename T>
+constexpr T MAX = std::numeric_limits<T>::max();
+#endif
+
+#ifdef LOCAL
+#define db(...) std::print(__VA_ARGS__)
+#define dbln(...) std::println(__VA_ARGS__)
+#else
+#define db(...)
+#define dbln(...)
+#endif
+//  }}}
+
+void solve() {
+  u64 x, y, n;
+  string s;
+  cin >> n >> x >> y >> s;
+
+  u32 ans = 0;
+
+  reverse(begin(s), end(s));
+
+  for (u32 i = 0; i < y; ++i) {
+    ans += (s[i] - '0') % 10 != 0;
+  }
+
+  ans += (s[y] - '0') != 1;
+  for (u32 i = y + 1; i < x; ++i) {
+    ans += (s[i] - '0') != 0;
+  }
+
+  println("{}", ans);
+}
+
+int main() {  // {{{
+  std::cin.exceptions(std::cin.failbit);
+#ifdef LOCAL
+  std::cerr.rdbuf(std::cout.rdbuf());
+  std::cout.setf(std::ios::unitbuf);
+  std::cerr.setf(std::ios::unitbuf);
+#else
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+#endif
+  u32 tc = 1;
+  // std::cin >> tc;
+  for (u32 t = 0; t < tc; ++t) {
+    solve();
+  }
+  return 0;
+}
+// }}}
